90c0ecc8117360e41f9b2e254e1270f2617f7746
mhyrr <grolsen@gmail.com>
2018-02-11 11:29:52 -0500

    Good working version

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background: #EEEEEE;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px; /* 5px rounded corners */
+  padding: 10px;
+`;
+
+
+
+export default Card
