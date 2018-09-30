d1801ed279e18ff4329abe00c57de28f569480c4
Greg Olsen <grolsen@gmail.com>
2018-09-30 08:35:09 -0400

    Main page done

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,19 +19,20 @@ const Header = (props) => (
                   <ul>
                     <li className="anim1"><p>Occasionally, I <i>do</i> things.</p></li>
                     <li className="anim2"><p>Occasionally, I <i>write</i> things.</p></li>
-                    <li className="anim3"><p>Occasionally, I <i>read</i> things.</p></li>
-                    <li className="anim4"><p>Occasionally, I <i>build</i> things.</p></li>
-                    <li className="anim5"><p>Occasionally, I <i>photograph</i> things.</p></li>
+                    <li className="anim3"><p>Occasionally, I <i>think</i> things.</p></li>
+                    <li className="anim4"><p>Occasionally, I <i>photograph</i> things.</p></li>
+                    <li className="anim5"><p>And sometimes, I'm just <i>beachin' it.</i></p></li>
                   </ul>
                 </div>
             </div>
         </div>
-        <nav>
+        <nav className="navAnim">
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li className="navanim2"><a href="javascript:;" onClick={() => {props.onOpenArticle('Blog')}}>Blog</a></li>
+                <li className="navanim3"><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Tumble</a></li>
+                <li className="navanim4"><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Photos</a></li>
+                <li className="navanim5"><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Beach</a></li>
+                <li className="navanim"><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
             </ul>
         </nav>
     </header>
