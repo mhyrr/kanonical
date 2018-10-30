98fbe75badba80a88c16c63b9c12432fcd112210
Greg Olsen <grolsen@gmail.com>
2018-10-30 08:06:15 -0400

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-101072268-1`,
       },
     },
     `gatsby-plugin-offline`,
