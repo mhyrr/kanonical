7b0d6ad6db1c928a11830ede43963d5733f5b0e8
Greg Olsen <grolsen@gmail.com>
2020-01-04 11:20:49 -0500

    Prototype flat

--- src/pages/tumble.js
+++ src/pages/tumble.js
@@ -407,6 +407,14 @@ class Tumble extends React.Component {
   }
 }
 
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function(depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+    }, []);
+  }
+});
+
 Tumble.propTypes = {
   route: React.PropTypes.object,
 }
