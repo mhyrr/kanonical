625f9e9bc4486435d901582c81883d37b7021bf0
Greg Olsen <grolsen@gmail.com>
2021-10-06 09:18:31 -0400

    Functioning header, image size needs to be more responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
-import GlobalStyle from '../globalstyles'
+import GlobalStyle from '../styles/globalstyles'
 
 
 const Layout = ({ location, title, children }) => {
---
+++ src/components/navigation.js
@@ -0,0 +1,36 @@
+import { Link } from "gatsby"
+import React, { useContext } from "react"
+import styled from 'styled-components';
+import { mainMenuItems } from "./menu-items"
+import { NavigationStyles } from "../styles/navstyles"
+import MenuContext from "./menucontext"
+
+const NavTitle = styled.h4`
+  font-size: var(--fontSize-7);
+  font-family: var(--sansSerif);
+  margin: 0;
+`;
+
+const Navigation = () => {
+  const [isOpen, setNav] = useContext(MenuContext)
+
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+
+  return (
+    <NavigationStyles className={isOpen ? "open" : "closed"}>
+      <ul>
+        {mainMenuItems.map((item, index) => (
+          <li key={`menu-item-${index}`} className="nav-link">
+            <Link to={item.path} onClick={toggleNav}>
+              <NavTitle>{item.title}</NavTitle>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </NavigationStyles>
+  )
+}
+
+export default Navigation
