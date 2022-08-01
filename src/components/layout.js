823e13f02616d5b7adb1a6763de4e756b6f5835e
Greg Olsen <grolsen@gmail.com>
2022-08-01 13:14:00 -0400

    dalle

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,31 +30,6 @@ const Layout = ({ location, title, children }) => {
 
         </div>
         <Footer></Footer>
-        <CookieConsent
-          location="bottom"
-          buttonText="Okay"
-          cookieName="kanonicalCookie"
-          expires={150}
-          style={{
-            background: "var(--black)",
-            padding: "var(--spacing)",
-            fontSize: "16px",
-            boxSizing: "border-box",
-          }}
-          buttonStyle={{
-            padding: "1rem",
-            color: "var(--black)",
-            backgroundColor: "#fff",
-            fontSize: "16px",
-          }}
-        >
-          This website uses cookies to help improve your experience. By using
-          this site you agree to the
-          <Link to="/privacy" style={{ color: "#fff" }}>
-            privacy statement
-          </Link>
-          .
-        </CookieConsent>
       </motion.div>
     </div>
   )
